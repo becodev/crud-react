82481875d5cbdd2876df9e6237c90d5a457c0381
Juan Pablo Loureyro <juanpabloloureyro@gmail.com>
2019-12-12 10:10:16 -0300

    avanzando

--- src/components/Router.js
+++ src/components/Router.js
@@ -2,16 +2,72 @@ import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import axios from 'axios';
 import Header from './Header';
-
+import Navegacion from './Navegacion';
+import Posts from './Posts';
+import SinglePost from './SinglePost';
 
 class Router extends Component {
+    constructor(){
+        super();
+        this.state = {
+            posts: []
+        }
+    };
 
+    componentDidMount() {
+        this.obtenerPosts();
+    }
+
+    obtenerPosts = () => {
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+        .then(res => {
+            this.setState({
+                posts: res.data
+            })
+        });
+    };
+
+    borrarPost = (id) => {
+        console.log(id)
+    }
+    
+    
     render() {
         return (
             <BrowserRouter>
                 <div className="container">
                     <div className="row justify-content-center">
                         <Header></Header>
+                        <Navegacion></Navegacion>
+                        <Switch>
+                            <Route exact path="/" render={
+                                () => (
+                                    <Posts 
+                                        posts={this.state.posts} 
+                                        borrarPost={this.borrarPost}
+                                        />
+                                )
+                            } ></Route>
+
+                            <Route exact path="/post/:postId" render= {
+                                (props) => {
+                                    let idPost = props.location.pathname.replace('/post/','');
+                                    const posts = this.state.posts;                            
+                                 
+                                    let filtro = posts.filter(post => (
+                                        post.id === Number(idPost)
+                                        
+                                    ));
+                                    
+                                    return(
+                                        <SinglePost
+                                            post={filtro[0]}
+                                        />
+                                        
+                                    )
+                                }
+                            }></Route>
+                        </Switch>
                     </div>
                 </div>
             
